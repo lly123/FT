74191696576782bdca4e7e4f704a296476f901bb
lly <lly123@gmail.com>
2012-10-19 15:53:58 +0800

    refactoring

--- One.rb
+++ One.rb
@@ -1,7 +1,6 @@
 require "./Zero"
 
 $_TYPE_OF = L{|n| $_0Car[n]}
-$_TYPE_EQ = L{|n| L{|m| $_0IF[$_0EQ[$_TYPE_OF[n]][$_TYPE_OF[m]]][$_TRUE][$_FALSE]}}
 
 _VAL = L{|n| $_0Cdr[n]}
 
@@ -13,19 +12,16 @@ $_IsNULL = L{|n| $_0IF[$_0EQ[$_0Car[n]][$_00]][$_TRUE][$_FALSE]}
 $_NUM = L{|n| $_0C[$_02][n]}
 $_IsNUM = L{|n| $_0IF[$_0EQ[$_02][$_TYPE_OF[n]]][$_TRUE][$_FALSE]}
 
-$_Add1 = L{|n| $_NUM[$_0Add1[_VAL[n]]]}
-$_Add = L{|n| L{|m| $_0IF[$_TYPE_EQ[n][m]][$_NUM[$_0Add[_VAL[n]][_VAL[m]]]][$_NULL]}}
+_BOTH_NUM = L{|n| L{|m| $_AND[$_IsNUM[n]][$_IsNUM[m]]}}
 
-$_0 = $_NUM[$_00]
-$_1 = $_NUM[$_01]
-$_2 = $_Add1[$_1]
-$_3 = $_Add1[$_2]
-$_4 = $_Add1[$_3]
-$_5 = $_Add1[$_4]
-$_6 = $_Add1[$_5]
-$_7 = $_Add1[$_6]
-$_8 = $_Add1[$_7]
-$_9 = $_Add1[$_8]
+$_Add1 = L{|n| $_IF[$_IsNUM[n]][$_NUM[$_0Add1[_VAL[n]]]][$_NULL] }
+$_Add = L{|n| L{|m| $_IF[_BOTH_NUM[n][m]][$_NUM[$_0Add[_VAL[n]][_VAL[m]]]][$_NULL]}}
+
+$_EQ = L{|n| L{|m| $_IF[_BOTH_NUM[n][m]][$_0EQ[_VAL[n]][_VAL[m]]][$_NULL]}}
+$_LT = L{|n| L{|m| $_IF[_BOTH_NUM[n][m]][$_0LT[_VAL[n]][_VAL[m]]][$_NULL]}}
+$_GT = L{|n| L{|m| $_IF[_BOTH_NUM[n][m]][$_0GT[_VAL[n]][_VAL[m]]][$_NULL]}}
+
+$_IsZero = L{|n| $_EQ[n][$_0]}
 
 # >> Type 3 : Boolean
 _BOOL = L{|n| $_0C[$_03][n]}
@@ -46,5 +42,14 @@ $_IsTUPLE = L{|n| $_0IF[$_0EQ[$_0Car[n]][$_01]][$_TRUE][$_FALSE]}
 $_FST = L{|n| $_0Car[$_0Cdr[n]]}
 $_SND = L{|n| $_0Cdr[$_0Cdr[n]]}
 
-
+$_0 = $_NUM[$_00]
+$_1 = $_NUM[$_01]
+$_2 = $_Add1[$_1]
+$_3 = $_Add1[$_2]
+$_4 = $_Add1[$_3]
+$_5 = $_Add1[$_4]
+$_6 = $_Add1[$_5]
+$_7 = $_Add1[$_6]
+$_8 = $_Add1[$_7]
+$_9 = $_Add1[$_8]
 
