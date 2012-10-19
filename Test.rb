3c1a966061d45b5877390e010baab6c9973c711a
lly <lly123@gmail.com>
2012-10-19 20:37:29 +0800

    add $_LIST_SUB

--- Test.rb
+++ Test.rb
@@ -9,7 +9,6 @@ $_Print0[ $_0IF[ $_0GT[$_02][$_02] ][ $_01 ][ $_00 ] ]
 
 p "--------"
 
-#$_LEN[$_LIST[$_TAIL[$_1]][$_HEAD]]
 $_Print0[$_IF[$_IsTUPLE[ $_T[$_1][$_NULL] ]][$_00][$_01]]
 $_Print[ $_LEN[$_LIST[$_TAIL[$_1]][$_2][$_HEAD]] ]
 
@@ -19,5 +18,7 @@ list = $_LIST[$_TAIL[$_2]][$_TRUE][$_5][$_HEAD]
 $_Print[$_LEN[list]][0]
 $_Print[$_LIST_AT[list][$_0]][0]
 $_PrintList[list][0]
-#$_Print[ $_Add[$_1][$_5] ]
 
+p "--------"
+list = $_LIST_SUB[list][$_1][$_FALSE]
+$_PrintList[list][0]
--- Two.rb
+++ Two.rb
@@ -8,11 +8,22 @@ $_LEN = $_Y[_LEN0]
 $_TAIL = L{|n| $_T[n][$_NULL]}
 $_HEAD = $_NULL
 
-_LIST0 = L{|s| L{|l| L{|p| $_IF[$_IsNULL[p]][l][s[$_T[p][l]]]}}}
+_LIST0 = L{|s| L{|l| L{|n| $_IF[$_IsNULL[n]][l][s[$_T[n][l]]]}}}
 $_LIST = $_Y[_LIST0]
 
 _LIST_AT0 = L{|s| L{|l| L{|n| $_IF[$_IsZero[n]][$_FST[l]][ L{|g| s[$_SND[l]][$_Sub1[n]][g]}]}}}
 $_LIST_AT = $_Y[_LIST_AT0]
 
+_LIST_SUB0 = L{|s| L{|l| L{|n| L{|m| L{|k|
+  $_IF[$_IsNULL[l]][$_NULL][
+      $_IF[$_EQ[n][m]][
+         L{|g| $_T[k][s[$_SND[l]][$_Sub1[n]][m][k]][g]}
+      ][
+         L{|g| $_T[$_FST[l]][s[$_SND[l]][$_Sub1[n]][m][k]][g]}
+      ]
+  ]
+}}}}}
+_LIST_SUB1 = $_Y[_LIST_SUB0]
+$_LIST_SUB = L{|l| L{|n| L{|m| _LIST_SUB1[l][$_LEN[l]][$_Add1[n]][m] }}}
 
 
